6dcc2f3345745cd338a3b9d34cff76fb5023036c
Norman Teibert <norman.teibert@gmail.com>
2017-04-17 01:24:32 +0200

    #4

--- src/hu/helixlab/homework/homework05/Sort.java
+++ src/hu/helixlab/homework/homework05/Sort.java
@@ -7,21 +7,19 @@ public class Sort implements ISort{
     @Override
     public void bubbleSort(int[] bubble){
 
-        int swaps = 0;
-        int swapsTemp = 0;
-
             for(int i = bubble.length; i > 1; --i){
+                int swaps = 0;
                 for(int j = 0; j < i-1; ++j){
 
                     if(bubble[j] > bubble[j + 1]){
                         int temp = bubble[j + 1];
                         bubble[j + 1] = bubble[j];
                         bubble[j] = temp;
-                        swapsTemp = swaps + 1;
+                        swaps = swaps + 1;
                     }
-                    if(swapsTemp == swaps){
-                        break;
-                    }
+                }
+                if(swaps == 0){
+                    break;
                 }
             }
             System.out.println("\t" + Arrays.toString(bubble));
