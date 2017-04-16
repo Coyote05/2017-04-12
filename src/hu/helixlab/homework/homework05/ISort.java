a1333bb7467275a2f62334e5cf37e89f70a59626
Norman Teibert <norman.teibert@gmail.com>
2017-04-16 23:25:28 +0200

    #1

--- src/hu/helixlab/homework/homework05/ISort.java
+++ src/hu/helixlab/homework/homework05/ISort.java
@@ -3,4 +3,13 @@ package hu.helixlab.homework.homework05;
 public interface ISort {
 
     void bubbleSort(int[] bubble);
+
+    void insertionSort();
+
+    void selectionSort();
+
+    void mergeSort();
+
+    void quickSort();
 }
+
